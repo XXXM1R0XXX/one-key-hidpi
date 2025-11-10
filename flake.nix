67a7ceaa993a1ef2d6fe245f4c32d392dac1e9bd
Богдан Кристиан <87042609+XXXM1R0XXX@users.noreply.github.com>
2025-11-10 13:18:38 -0800

    Refactor flake.nix to simplify package creation

--- flake.nix
+++ flake.nix
@@ -5,36 +5,36 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
-    # Darwin module for nix-darwin integration
-    darwinModules.default = import ./darwin-module.nix;
-    
-    # Package for display-info script
-    packages = nixpkgs.lib.genAttrs [ "x86_64-darwin" "aarch64-darwin" ] (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        display-info = pkgs.stdenv.mkDerivation {
-          pname = "display-info";
-          version = "1.0.0";
-          
-          src = ./.;
-          
-          installPhase = ''
-            mkdir -p $out/bin
-            cp ${./display-info.sh} $out/bin/display-info
-            chmod +x $out/bin/display-info
-          '';
-          
-          meta = with pkgs.lib; {
-            description = "Display information utility for macOS";
-            platforms = platforms.darwin;
-          };
-        };
-        
-        default = self.packages.${system}.display-info;
-      }
-    );
-  };
+  outputs = { self, nixpkgs }:
+    let
+      # Определяем поддерживаемые системы один раз, чтобы избежать повторений
+      supportedSystems = [ "x86_64-darwin" "aarch64-darwin" ];
+
+      # Создаем pkgs для каждой системы
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+    in
+    {
+      # Модуль для nix-darwin остается без изменений
+      darwinModules.default = import ./darwin-module.nix;
+
+      # Пакеты для каждой поддерживаемой системы
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          # Используем writeShellScriptBin для простой упаковки скрипта.
+          # Это намного проще и эффективнее, чем mkDerivation.
+          display-info = pkgs.writeShellScriptBin "display-info" (
+            # Встраиваем содержимое файла display-info.sh прямо в пакет.
+            # Nix будет автоматически отслеживать изменения в этом файле.
+            builtins.readFile ./display-info.sh
+          );
+        });
+
+      # Определяем пакет по умолчанию для каждой системы отдельно,
+      # чтобы избежать ошибки бесконечной рекурсии.
+      defaultPackage = forAllSystems (system: self.packages.${system}.display-info);
+    };
 }
